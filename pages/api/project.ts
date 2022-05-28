6ed15405f29c08df8446029e43610ee61d7f5a6b
Rodrigo Casale <rodrigocasale@192.168.1.22>
2022-05-28 14:37:33 -0500

    Fetch project by id endpoint added

---
+++ pages/api/project.ts
@@ -0,0 +1,27 @@
+import { FETCH_PROJECT_BY_ID } from '@/apollo/gql/gqlProjects';
+import { client } from '@/apollo/apolloClient';
+
+const fetchProject = async (id: number) => {
+	const { data: project } = await client.query({
+		query: FETCH_PROJECT_BY_ID,
+		variables: {
+			id: id,
+		},
+	});
+	const { projectById } = project;
+	if (projectById) {
+		return projectById;
+	}
+};
+
+const handler = async (req: any, res: any) => {
+	const { query } = req;
+	const { id } = query;
+	try {
+		res.status(200).json({ projects: await fetchProject(parseInt(id)) });
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+};
+
+export default handler;
