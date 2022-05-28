84b47aadfcf1afc6e3cbcc049d62696af6a2f47d
Rodrigo Casale <rodrigocasale@192.168.1.22>
2022-05-28 14:42:24 -0500

    Fetch users endpoint added

---
+++ pages/api/user.ts
@@ -0,0 +1,28 @@
+import { GET_USER_BY_ADDRESS } from '@/apollo/gql/gqlUser';
+import { client } from '@/apollo/apolloClient';
+
+const fetchUser = async (address: string) => {
+	const { data: userData } = await client.query({
+		query: GET_USER_BY_ADDRESS,
+		variables: {
+			address: address,
+		},
+	});
+
+	const { userByAddress } = userData;
+	if (userByAddress) {
+		return userByAddress;
+	}
+};
+
+const handler = async (req: any, res: any) => {
+	const { query } = req;
+	const { address } = query;
+	try {
+		res.status(200).json({ projects: await fetchUser(address) });
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+};
+
+export default handler;
